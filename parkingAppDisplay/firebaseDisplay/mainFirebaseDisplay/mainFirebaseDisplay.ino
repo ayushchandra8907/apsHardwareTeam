972f41d8af61cde661fd09bf9ca5c85637e4841f
waknuckles1020 <71286983+ayushchandra8907@users.noreply.github.com>
2023-08-02 04:12:04 -0500

    most merging complete

--- parkingAppDisplay/firebaseDisplay/mainFirebaseDisplay/mainFirebaseDisplay.ino
+++ parkingAppDisplay/firebaseDisplay/mainFirebaseDisplay/mainFirebaseDisplay.ino
@@ -1,19 +1,16 @@
 #include <ESP8266WiFi.h>                                               
 #include <FirebaseArduino.h>                                        
- 
+
+
+//FIREBASE====================================================================================
 #define FIREBASE_HOST "apsparkingboardtest-default-rtdb.firebaseio.com"              // the project name address from firebase id
 #define FIREBASE_AUTH "xg6KvcccYzCzYCS2TPhRbisbzKzh14iOcumbSGin"       // the secret key generated from firebase
 #define WIFI_SSID "APS1057"                                          
 #define WIFI_PASSWORD "!APS1057!"                                  
  
 String fireStatus = "";  // led status received from firebase
-int led = 5;  
-                                                              
-void setup() 
-{
-  Serial.begin(9600);
-  delay(1000);    
-  pinMode(led, OUTPUT);                 
+
+void connectBoard(){
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);                               
   Serial.print("Connecting to ");
   Serial.print(WIFI_SSID);
@@ -25,25 +22,122 @@ void setup()
   Serial.println();
   Serial.print("Connected to ");
   Serial.println(WIFI_SSID);
-  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);                  // connect to firebase
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);                  
+}
+
+//arrays===============================================================
+int pinsArr[2][4] = {
+  {7, 6, 5, 4}, 
+  {13, 12, 11, 10}
+};
+
+bool configArr[2][4] = {
+  {true, true, true, true},
+  {true, true, true, true}
+};
+
+String firebaseArr[2][4] = {
+  {Firebase.getString("SPACE_1"), Firebase.getString("SPACE_2"), Firebase.getString("SPACE_3"), Firebase.getString("SPACE_4")},
+  {Firebase.getString("SPACE_5"), Firebase.getString("SPACE_6"), Firebase.getString("SPACE_7"), Firebase.getString("SPACE_8")}
+};
+
+
+//BOARD INIT===================================================================================
+void togPin(int pinNum, bool on){
+  if(on){
+    digitalWrite(pinNum, HIGH);
+  } else {
+    digitalWrite(pinNum, LOW);
+  }
+}
+
+//sets all pins to either off or on----------------------------------
+void setAllPins(bool on){
+  for(int r = 0; r<2; r++){
+    for(int c = 0; c<4; c++){
+      togPin(pinsArr[r][c], on);
+    }
+  }
+}
+
+void setAllFirebase(bool on){
+  int i = 1;
+  for(int r = 0; r<2; r++){
+    for(int c = 0; c<4; c++){
+      String f = "SPACE_" + i;
+      if(on){
+        Firebase.setString(f, "ON");        
+      } else {
+        Firebase.setString(f, "OFF");        
+      }
+    }
+  }
+}
+
+//updates irl lot based off config--------------------------------------
+void updateLot(){
+  for(int r = 0; r<2; r++){
+    for(int c = 0; c<4; c++){
+      togPin(pinsArr[r][c], configArr[r][c]);
+    }
+  }
+}
+
+//updates firebase lot by reading config array
+void updateFirebaseLot(){
+  int i = 1;
+  for(int r = 0; r<2; r++){
+    for(int c = 0; c<4; c++){
+      String f = "SPACE_" + i;
+
+      if(configArr[r][c] == true){
+        Firebase.setString(f, "ON");
+      } else {
+        Firebase.setString(f, "OFF");        
+      }
+      
+      i++;
+    }
+  }
+}
+
+
+
+void setup() 
+{
+  Serial.begin(9600);
+  delay(1000);    
+
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(4, OUTPUT);
+  
   Firebase.setString("LED_STATUS", "OFF");                       //send initial string of led status
 }
  
 void loop() 
 {
-  fireStatus = Firebase.getString("LED_STATUS");                                      // get ld status input from firebase
-  if (fireStatus == "ON") 
-  {                                                          // compare the input of led status received from firebase
-    Serial.println("Led Turned ON");                                                        
-    digitalWrite(led, HIGH);                                                         // make external led ON
-  } 
-  else if (fireStatus == "OFF") 
-  {                                                  // compare the input of led status received from firebase
-    Serial.println("Led Turned OFF");
-    digitalWrite(led, LOW);                                                         // make external led OFF
-  }
-  else 
-  {
-    Serial.println("Command Error! Please send ON/OFF");
-  }
+//  fireStatus = Firebase.getString("SPACE_1");                                      // get ld status input from firebase
+//  if (fireStatus == "ON") 
+//  {                                                          // compare the input of led status received from firebase
+//    Serial.println("Led Turned ON");                                                        
+//    digitalWrite(led, HIGH);                                                         // make external led ON
+//  } 
+//  else if (fireStatus == "OFF") 
+//  {                                                  // compare the input of led status received from firebase
+//    Serial.println("Led Turned OFF");
+//    digitalWrite(led, LOW);                                                         // make external led OFF
+//  }
+//  else 
+//  {
+//    Serial.println("Command Error! Please send ON/OFF");
+//  }
+
+  
+
 }
